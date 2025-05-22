ff69760f7dc0ec78e5844b31ba98dfe8567e9f46
hamidou dioumanara <hamidoudioumanara99@gmail.com>
2025-05-22 11:50:53 +0200

    f

--- CODE-WALLET01/src/preload/index.js
+++ CODE-WALLET01/src/preload/index.js
@@ -1,4 +1,4 @@
-import { contextBridge } from 'electron'
+// import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
@@ -18,3 +18,24 @@ if (process.contextIsolated) {
   window.electron = electronAPI
   window.api = api
 }
+const { contextBridge, ipcRenderer } = require('electron');
+
+
+//  On expose proprement les fonctions dans window.electronAPI
+contextBridge.exposeInMainWorld('electronAPI', {
+  sendFragment: (fragment) => ipcRenderer.send('add-fragment', fragment),
+  getFragments: () => ipcRenderer.invoke('get-fragments'),
+  deleteFragment: (id) => ipcRenderer.send('delete-fragment', id),
+  editFragment: (fragment) => ipcRenderer.send('edit-fragment', fragment),
+// Ajout pour les tags :
+editTag: (oldName, newName) => ipcRenderer.send('edit-tag', { oldName, newName }),
+deleteTag: (name) => ipcRenderer.send('delete-tag', name),
+importFragments: (fragments) => ipcRenderer.send('import-fragments', fragments),
+});
+
+
+
+
+
+// Juste pour verifier que preload marche
+console.log(' preload.js chargé et exécuté');
